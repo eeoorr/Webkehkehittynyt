07730d6a8f57c3797f61aa96206c62dba5226e0c
Name <aaa@asd.fi>
2026-03-15 16:51:43 +0200

    "task f completed   "

--- BookingSystem/Phase5/src/db/pool.js
+++ BookingSystem/Phase5/src/db/pool.js
@@ -1,5 +1,18 @@
 // src/db/pool.js
 import pg from "pg";
 const { Pool } = pg;
-const pool = new Pool({});
+
+
+const pool = new Pool({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+});
+
+
+
+
+
 export default pool;
